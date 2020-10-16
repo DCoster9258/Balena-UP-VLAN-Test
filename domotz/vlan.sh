2da2c8a798f731efa1027fa6f03b9b8bdfd2a066
DCoster9258 <70751484+DCoster9258@users.noreply.github.com>
2020-10-16 10:35:18 +0100

    Testing Variables

--- domotz/vlan.sh
+++ domotz/vlan.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
-
+IP=192.168.0.100
+NETMASK=255.255.255.0
+INTERFACE=enp2s0
+VLANID=20
+VLANINTERFACE=enp2s0.20
 echo "Modprobe 1 to fail"
 modprobe 8021q || true
 sleep 1
 
 echo "vconfig 1 to fail"
-vconfig add enp2s0 20 || true
+vconfig add $INTERFACE $VLANID || true
 sleep 1
 
 echo "Modprobe should work"
@@ -14,21 +18,21 @@ modprobe 8021q || true
 sleep 1
 
 echo "Creating Interface if not pre-existing"
-vconfig add enp2s0 20 || true
+vconfig add $INTERFACE $VLANID || true
 sleep 1
 
 echo "IP Assign 1"
-ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
+ifconfig $VLANINTERFACE $IP netmask $NETMASK up
 sleep 10
 
 echo "IP Assign 2"
-ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
+ifconfig $VLANINTERFACE $IP netmask $NETMASK up
 (
 while true
 do
 	sleep 60
 	echo "IP Loop"
-	ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
+	ifconfig $VLANINTERFACE $IP netmask $NETMASK up
 done
 )
 exit 0
