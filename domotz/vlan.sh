f1864f4ad3b4ea027b4e4c4c40d2eb81939055f8
DCoster9258 <70751484+DCoster9258@users.noreply.github.com>
2020-10-16 08:42:18 +0100

    Added ifconfig on loop

--- domotz/vlan.sh
+++ domotz/vlan.sh
@@ -24,3 +24,11 @@ sleep 1
 echo "IP Assign 2"
 ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
 sleep 1
+
+run=0
+while [ $run -le 2 ]
+do
+	echo "IP Assign 2"
+	ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
+	sleep 10
+done
