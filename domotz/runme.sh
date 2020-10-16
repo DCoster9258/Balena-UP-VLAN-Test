fdef418d20d0708b3105cce37c4700ee3dde0d04
DCoster9258 <70751484+DCoster9258@users.noreply.github.com>
2020-10-16 08:31:41 +0100

    Initial

--- domotz/runme.sh
+++ domotz/runme.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 echo "The DomotzPro agent is listening on port 3000. You can connect to it using your browser."
+
+
 while true; do sleep 1000; done
---
+++ domotz/vlan.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+echo "Modprobe 1 to fail"
+modprobe 8021q || true
+sleep 1
+
+echo "vconfig 1 to fail"
+vconfig add enp2s0 20 || true
+sleep 1
+
+echo "Modprobe should work"
+modprobe 8021q || true
+sleep 1
+
+echo "Creating Interface"
+vconfig add enp2s0 20 || true
+sleep 1
+
+echo "IP Assign 1"
+ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
+sleep 1
+
+echo "IP Assign 2"
+ifconfig enp2s0.20 192.168.0.100 netmask 255.255.255.0 up
+sleep 1
